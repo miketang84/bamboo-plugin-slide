3884774bfdd6cf13c6232eb62d707dda0a4e2161
Daogang Tang <daogangtang@gmail.com>
2012-07-16 14:49:10 +0800

    change to work.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- init.lua
+++ init.lua
@@ -1,23 +1,29 @@
 module(..., package.seeall)
 
 local TMPLS = {
-	['basic'] = '../plugins/slider/views/basic.html'
+	['basic'] = '../plugins/slide/views/basic.html'
 
 }
 
 --[[
-{^ slider _tag="xxx",
+
+datasource's element shall be the form as follows:
+1. element.url;
+2. element.title;
+3. element.path;
+4. element.thumb_path;
+
+
+
+{^ slide _tag="xxx",
 	datasource = xxxx,
-	urlprefix = xxx,
 	
 
 ^}
 --]]
 function main(args, env)
-	assert(args._tag, '[Error] @plugin board - missing _tag.')
-	assert(args.title, '[Error] @plugin board - missing title.')
-	assert(args.body, '[Error] @plugin board - missing body.')
-	
+	assert(args._tag, '[Error] @plugin slide - missing _tag.')
+
 	local tmpl = args.tmpl and TMPLS[args.tmpl] and args.tmpl or 'basic'
 	
 	return View(TMPLS[tmpl])(args)
